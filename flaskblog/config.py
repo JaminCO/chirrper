ed6b8aea7530a9173801f05147493610deb836eb
Jamin Onuegbu <76646536+JaminCO@users.noreply.github.com>
2022-12-16 00:07:00 +0100

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,15 +5,11 @@ load_dotenv()
 
 class Config:
 	SECRET_KEY = os.getenv("SECRET_KEY")
-	# 'fbf21f273c2b6864d2d3ee1e9ad6014f'
 	SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-	# 'sqlite:///data.db'
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-	# "chirrperapp@gmail.com"
 	MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-	# "xwtj hbkp syyx drum"
 	SQLALCHEMY_TRACK_MODIFICATIONS=True
 	# os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
